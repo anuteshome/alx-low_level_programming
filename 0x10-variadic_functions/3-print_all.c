28d3795383e0e8cea678e71d4286a8e1ceac9f30
anuteshome <ananyateshome2@gmail.com>
2023-01-18 11:26:18 -0800

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,10 +2,6 @@
 
 /**
  * print_all - prints anything
-#include "variadic_functions.h"
-
-/**
- * print_all - prints anything
  * @format: list of types of arguments passed to the function
  */
 void print_all(const char * const format, ...)
