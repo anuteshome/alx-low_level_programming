351078e4d86cc13280f3bf5e0312942a1bd302f6
anuteshome <ananyateshome2@gmail.com>
2023-01-18 11:24:36 -0800

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -22,4 +22,12 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (!separator)
 			printf("%s", str);
 		else if (separator && i == 0)
+			printf("%s", str);
+		else
+			printf("%s%s", separator, str);
+	}
 
+	printf("\n");
+
+	va_end(list);
+}
